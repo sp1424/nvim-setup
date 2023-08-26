4f96c145b4d3f8e808a7d5c4343be013a9855c08
Sebastian Pira <sebastianpira@sebastians-air.home>
2023-08-26 21:53:38 +0100

    add live grep

--- init.lua
+++ init.lua
@@ -1,25 +1,28 @@
 require 'plugins.floatterm'
 require 'plugins.treesitter'
 require 'plugins.nerdtree'
+require 'plugins.telescope'
 
 local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
 Plug('catppuccin/nvim', {
-    as = 'catpuccin'
+	as = 'catpuccin'
 })
 Plug('nvim-treesitter/nvim-treesitter', {
-    run = ':TSUpdate'
+	run = ':TSUpdate'
 })
 Plug('numToStr/FTerm.nvim')
 Plug('folke/which-key.nvim')
 Plug('neoclide/coc.nvim', {
-    branch = 'release'
+	branch = 'release'
 })
 Plug('Rigellute/shades-of-purple.vim')
 Plug('preservim/nerdtree')
 Plug('ryanoasis/vim-devicons')
 Plug('nvim-telescope/telescope.nvim')
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope-live-grep-args.nvim')
 
 vim.call('plug#end')
 
@@ -30,13 +33,17 @@ local set = vim.opt
 set.tabstop = 4
 set.shiftwidth = 4
 set.softtabstop = 0
-set.encoding='UTF-8'
+set.encoding = 'UTF-8'
 
 vim.g.mapleader = ' '
 vim.api.nvim_command('inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm() : "\\<CR>"')
 vim.api.nvim_command('inoremap <silent><expr> <ESC> coc#pum#visible() ? coc#pum#cancel() : "\\<ESC>"')
 vim.keymap.set('n', 'gd', '<Plug>(coc-definition)')
 vim.keymap.set('n', 'gr', '<Plug>(coc-references)')
+vim.keymap.set('n', '<leader>ff', "<CMD> lua require('telescope.builtin').find_files() <CR>")
+vim.keymap.set('n', '<leader>fg', "<CMD> lua require('telescope').extensions.live_grep_args.live_grep_args() <CR>")
+vim.keymap.set('n', '<leader>fb', "<CMD> lua require('telescope.builtin').buffers() <CR>")
+vim.keymap.set('n', '<leader>fh', "<CMD> lua require('telescope.builtin').help_tags() <CR>")
 
 vim.cmd [[autocmd BufWritePre *.go :call CocAction('runCommand', 'editor.action.organizeImport')]]
 vim.cmd [[autocmd BufWritePost * :call CocAction('format')]]
@@ -44,4 +51,4 @@ vim.cmd [[autocmd BufWritePost * :call CocAction('format')]]
 setupTerminal()
 setupTreesitter()
 setupNerdtree()
---setupTelescope()
+setupTelescope()
--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,1 +1,7 @@
-
+function setupTelescope()
+	telescope = require('telescope')
+	telescope.load_extension("live_grep_args")
+	telescope.setup {
+		file_ignore_patterns = { 'node_modules', 'vendor' }
+	}
+end
